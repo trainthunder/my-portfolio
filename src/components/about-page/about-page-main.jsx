d9682d8733d7de5ed64525138e10bb9d08b716d9
trainthunder <kitikun.po@gmail.com>
2024-09-14 16:06:45 +0700

    test: about-page

---
+++ src/components/about-page/about-page-main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SocialNavigate from "../social-navigate";
+
+function AboutPageMain() {
+  return (
+    <section className="w-screen h-screen pt-[60px] md:pt-[66px] flex items-center justify-center relative">
+      <SocialNavigate />
+      <div className=""></div>
+    </section>
+  );
+}
+
+export default AboutPageMain;
